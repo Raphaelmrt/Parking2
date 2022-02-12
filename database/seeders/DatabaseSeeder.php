51ca336f0f3c334420df858efc5c7247cc583495
sly987 <76872198+sly987@users.noreply.github.com>
2022-02-12 12:53:25 +0100

    création d'un user et d'un admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        User::create([
+            'name'=>'admin',
+            'surname'=>'admin',
+            'email'=>'admin@mail.fr',
+            'admin'=>1,
+            'email_verified_at' => now(),
+            'password' => Hash::make('12345678'), // password
+            'remember_token' => Str::random(10),
+        ]);
+        User::create([
+            'name'=>'user',
+            'surname'=>'user',
+            'email'=>'user@mail.fr',
+            'admin'=>0,
+            'email_verified_at' => now(),
+            'password' => Hash::make('12345678'), // password
+            'remember_token' => Str::random(10),
+        ]);
         // \App\Models\User::factory(10)->create();
     }
 }
