ffb6c2da31df060281babdc2573882ba6af5c634
Raphaelmrt <90179376+Raphaelmrt@users.noreply.github.com>
2022-02-14 17:44:50 +0100

    Modification Seeder
    création de la commande de création de place et d'user dans la BDD

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -34,6 +34,7 @@ class DatabaseSeeder extends Seeder
             'password' => Hash::make('12345678'), // password
             'remember_token' => Str::random(10),
         ]);
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(10)->create();
+         \App\Models\Place::factory(10)->create();
     }
 }
