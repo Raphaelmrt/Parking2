8a894e8730529a7773ce2201f3a92b909d3776c9
Raphaelmrt <90179376+Raphaelmrt@users.noreply.github.com>
2022-03-14 17:54:59 +0100

    page ajouter place

---
+++ resources/views/admin/admin_addPlace.blade.php
@@ -0,0 +1,32 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Créer une place') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+
+                    <form action="{{route('Place.create')}}" method="POST">
+                        @csrf
+                        <input type="radio" name="handicapé" value="Handicapé">
+                        <input type="radio" name="nonHandicapé" value="Normal">
+
+                        <input type="submit" value="Valider"> 
+                    </form>
+
+
+
+
+
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
