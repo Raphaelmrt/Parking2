fba461f9e671d3087bbb27f1a3a5ce2ba784bdff
sly987 <76872198+sly987@users.noreply.github.com>
2022-03-21 13:20:46 +0100

    ajout de rangfile

--- database/migrations/2022_02_12_105513_alter_user.php
+++ database/migrations/2022_02_12_105513_alter_user.php
@@ -14,7 +14,8 @@ class AlterUser extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('surname');          
+            $table->string('surname');
+            $table->integer('rangFile')->default(0);          
             $table->boolean('admin')->default(0);
         });
     }
