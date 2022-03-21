a7f9f893bc8d8889e06e74c654ab61726a510338
Raphaelmrt <90179376+Raphaelmrt@users.noreply.github.com>
2022-03-21 14:50:14 +0100

    bouton ajouter une place

--- resources/views/admin/admin_ListPlace.blade.php
+++ resources/views/admin/admin_ListPlace.blade.php
@@ -7,6 +7,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <a href="{{route('Place.create')}}"><button class="btn">Ajouter une place</button></a>
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     Liste des places
--- resources/views/admin/admin_addPlace.blade.php
+++ resources/views/admin/admin_addPlace.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Créer une place') }}
+            {{ __('Ajouter une place') }}
         </h2>
     </x-slot>
 
@@ -10,10 +10,13 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
 
-                    <form action="{{route('Place.create')}}" method="POST">
+                    <form action="{{route('Place.store')}}" method="POST">
                         @csrf
-                        <input type="radio" name="nonHandicapé" value="Normal" checked>
-                        <input type="radio" name="handicapé" value="Handicapé">
+                        <H4>La place est-elle pour Handicapé ?</H4>
+                        <br><label for="nonHandicapé">Non
+                            <input type="radio" name="nonHandicapé" value="Normal" checked><br>
+                        <label for="handicapé">Oui    
+                            <input type="radio" name="handicapé" value="Handicapé"><br>
                         
 
                         <input type="submit" value="Valider"> 
